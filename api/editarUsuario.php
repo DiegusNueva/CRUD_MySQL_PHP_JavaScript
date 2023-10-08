363c14466c9b265b4e89da578ae49d434138c9fc
Diego Alonso Molina <diegoalonso@tuniversformacion.es>
2023-10-08 10:46:55 +0200

    Cambio del editarUsuario.php

--- api/editarUsuario.php
+++ api/editarUsuario.php
@@ -1,11 +1,19 @@
 <?php
+
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
+    require "conexion.php";
+
+    $json = file_get_contents("php://input");
+
+    $objEmpleado = json_decode($json);
+
+    $sql = "UPDATE usuarios SET usuario='$objEmpleado->usuario', contrasena='$objEmpleado->contrasena', email='$objEmpleado->email' WHERE idUsuario='$objEmpleado->idUsuario'";
     
-    // Variables de la conexion a la DB
-    $mysqli = new mysqli("localhost","root","root","pruebas");
-    
-    // Comprobamos la conexion
-    if($mysqli->connect_errno) {
-        die("Fallo la conexion");
-    } else {
-        //echo "Conexion exitosa";
-    }
+    $query = $mysqli->query($sql);
+
+    $jsonRespuesta = array('msg' => 'OK');
+    echo json_encode($jsonRespuesta);
